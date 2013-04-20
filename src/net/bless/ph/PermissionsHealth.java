869504a57ca9427715dde3c6e165890f6a745e5f
Zarius <zariust@gmail.com>
2013-04-21 00:28:20 +1000

    Major: update config layout to allow unlimited permission nodes and simplify config reading.  Consolidated command classes into single command manager.

--- src/net/bless/ph/PermissionsHealth.java
+++ src/net/bless/ph/PermissionsHealth.java
@@ -1,20 +1,21 @@
 package net.bless.ph;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.Logger;
-import net.bless.ph.OtherHP;
-import net.bless.ph.PHeal;
-import net.bless.ph.NodeCommand;
-import net.bless.ph.PermissionsHealth;
-import net.bless.ph.Reload;
-import net.bless.ph.LookUp;
-import net.bless.ph.ViewHP;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PermissionsHealth extends JavaPlugin implements Listener {
 	public static PermissionsHealth plugin;
+    public static Map<String, Integer> permissionsMap = new HashMap<String, Integer>();
+    
 	public final Logger logger = Logger.getLogger("MineCraft");
 
 
@@ -33,32 +34,44 @@ public class PermissionsHealth extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
-		getServer().getConsoleSender().sendMessage(
-				ChatColor.RED + "+------------------------------------------+");
-		getServer().getConsoleSender().sendMessage(
-				ChatColor.RED + "|    " + ChatColor.WHITE
-						+ "PermissionsHealth " + getDescription().getVersion()
-						+ " - Enabled" + ChatColor.RED + "    |");
-		getServer().getConsoleSender().sendMessage(
-				ChatColor.RED + "+------------------------------------------+");
-
 		this.saveDefaultConfig();
+		PHConfig.load(this.getConfig());
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(new HPListener(this), this);
 
-		getCommand("pheal").setExecutor(new PHeal(this));
-		getCommand("phc").setExecutor(new ViewHP(this));
-		getCommand("hp").setExecutor(new ViewHP(this));
-		getCommand("pho").setExecutor(new OtherHP(this));
-		getCommand("ph").setExecutor(new Reload(this));
-		getCommand("phl").setExecutor(new LookUp(this));
-		getCommand("phn").setExecutor(new NodeCommand(this));
-		getCommand("phh").setExecutor(new HealthCommand(this));
-		getCommand("phelp").setExecutor(new Help(this));
+		// This should be the only one we need.
+        getCommand("ph").setExecutor(new PHCommands(this));
+	//	getCommand("phl").setExecutor(new LookUp(this));
 		
+		// I would recommend just:
+		//getServer().getConsoleSender().sendMessage(getDescription().getName()+" "+getDescription().getVersion()+" loaded.");
 		
-
+        getServer().getConsoleSender().sendMessage(
+                ChatColor.RED + "+------------------------------------------+");
+        getServer().getConsoleSender().sendMessage(
+                ChatColor.RED + "|    " + ChatColor.WHITE
+                        + "PermissionsHealth " + getDescription().getVersion()
+                        + " - Enabled" + ChatColor.RED + "    |");
+        getServer().getConsoleSender().sendMessage(
+                ChatColor.RED + "+------------------------------------------+");
 
 	}
+
+    /** PermissionsHealth - getMaxHealth(playerName)
+     *  Gets the configured MaxHealth of given player.
+     *  
+     * @param playerName Exact name of player, must be online
+     * @return MaxHealth that would be set, null if no permission found or player doesn't exist
+     */
+    public Integer getMaxHealth(String playerName) {
+        for (Entry<String, Integer> entry  : PermissionsHealth.permissionsMap.entrySet()) {
+            Player player = Bukkit.getPlayerExact(playerName);
+            
+            if (player.hasPermission(entry.getKey())) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
 
 }
